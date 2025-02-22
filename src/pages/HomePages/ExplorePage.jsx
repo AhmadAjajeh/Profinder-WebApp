6a88a99ffc9df4c2dfcce5b7eb03239837a3a248
Ahmad-Ajajeh <secretajajeh356@gmail.com>
2025-02-22 18:15:50 +0300

    layout enhanced

--- src/pages/HomePages/ExplorePage.jsx
+++ src/pages/HomePages/ExplorePage.jsx
@@ -8,32 +8,33 @@ import ImageSliderModal from '../../components/general-ui/ImageSliderModal';
 
 export default function ExplorePage() {
   return (
-    <div className="w-full flex flex-row space-x-5 rtl:space-x-reverse">
+    <>
       {/* middle section */}
-      <div className=" flex flex-col item-center w-full lg:min-w-[500px] ">
-        {/* <HomeNavigation /> */}
-        <HomeNavigation />
+      <div className="flex-1 overflow-hidden min-w-[400px]">
+        <div className=" flex flex-col item-center ">
+          {/* <HomeNavigation /> */}
+          <HomeNavigation />
 
-        {/* content */}
-        <motion.div
-          className="flex flex-col space-y-4"
-          variants={{
-            hidden: { opacity: 0, y: -15 },
-            animate: { opacity: 1, y: 0 },
-          }}
-          initial="hidden"
-          animate="animate"
-          exit="hidden"
-        >
-          <NewPost />
-          <PostScroll />
-        </motion.div>
+          {/* content */}
+          <motion.div
+            className="flex flex-col space-y-4"
+            variants={{
+              hidden: { opacity: 0, y: -15 },
+              animate: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate="animate"
+            exit="hidden"
+          >
+            <NewPost />
+            <PostScroll />
+          </motion.div>
+        </div>
       </div>
-
       {/* far right section */}
       <div className="hidden h-fit lg:flex w-full lg:min-w-[250px] lg:max-w-[380px] xl:min-w-[430px] xl:max-w-[430px] sticky top-20">
         <ComplainAndSuggestion />
       </div>
-    </div>
+    </>
   );
 }
--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -77,7 +77,7 @@ export default function MainLayout() {
   }, [error, alert, dispatch]);
 
   return (
-    <>
+    <div>
       <AnimatePresence>
         {error.code !== 0 && (
           <ErrorModal
@@ -99,15 +99,15 @@ export default function MainLayout() {
       </header>
 
       {/* the down section */}
-      <div className="mx-auto pt-20 min-h-screen p-6 flex flex-row  md:space-x-5 rtl:space-x-reverse lg:px-10 xl:px-16  transition-all duration-300 ">
+      <div className="mx-auto pt-20 min-h-screen p-6 flex md:gap-5 rtl:space-x-reverse lg:px-10 xl:px-16  transition-all duration-300 ">
         {/* far left section */}
-        <div className="hidden md:inline h-80 md:w-full md:min-w-[230px] md:max-w-[230px] lg:min-w-[200px] lg:max-w-[240px] xl:min-w-[240px] xl:max-w-[240px] sticky top-20">
+        <aside className="hidden md:inline h-80 md:w-full md:min-w-[230px] md:max-w-[230px] lg:min-w-[200px] lg:max-w-[240px] xl:min-w-[240px] xl:max-w-[240px] sticky top-20">
           <SideNavigation />
-        </div>
+        </aside>
 
         <Outlet />
       </div>
-    </>
+    </div>
   );
 }
 
